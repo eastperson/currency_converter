569cbd6a09627a48641142417877d4cb37be4dba
EP <kjuioqq8@gmail.com>
2021-10-02 13:16:09 +0900

    CHORE: 개발 환경 구축

--- core/src/main/java/com/wirebarley/core/properties/CurrencyLayerProperties.java
+++ core/src/main/java/com/wirebarley/core/properties/CurrencyLayerProperties.java
@@ -1,2 +1,18 @@
-package com.wirebarley.core.properties;public class CurrencyLayerProperties {
+package com.wirebarley.core.properties;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@EnableConfigurationProperties
+@Getter
+@PropertySource("classpath:properties/currency_layer.properties")
+public class CurrencyLayerProperties {
+
+    @Value("${access-key}")
+    private String accessKey;
+
 }
--- front/src/main/java/com/wirebarley/front/FrontApplication.java
+++ front/src/main/java/com/wirebarley/front/FrontApplication.java
@@ -3,7 +3,10 @@ package com.wirebarley.front;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.wirebarley.core.*",
+        "com.wirebarley.front.*"
+})
 public class FrontApplication {
 
     public static void main(String[] args) {
