b3497b21a1c194b73f19099d6de76da0c2309f93
EP <kjuioqq8@gmail.com>
2021-10-04 09:53:56 +0900

    FIX: Server 실행시 최초 api 호출

--- api/src/main/java/com/wirebarley/api/scheduler/CurrencyScheduler.java
+++ api/src/main/java/com/wirebarley/api/scheduler/CurrencyScheduler.java
@@ -2,11 +2,15 @@ package com.wirebarley.api.scheduler;
 
 import com.wirebarley.api.service.CurrencyConvertService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CurrencyScheduler {
 
     private final CurrencyConvertService currencyConvertService;
@@ -16,6 +20,15 @@ public class CurrencyScheduler {
      */
     @Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
     public void dailySaveCurrentCurrency(){
+        currencyConvertService.saveCurrentCurrency();
+    }
+
+    /**
+     * 서버 실행시 최초 api 호출
+     */
+    @PostConstruct
+    public void initMessage() {
+        log.info("Init Api Call");
         currencyConvertService.saveCurrentCurrency();
     }
 
